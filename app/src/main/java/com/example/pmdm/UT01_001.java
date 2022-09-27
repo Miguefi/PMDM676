1566631bf53fe027823a0b81236631b3317a22ae
at6DAM2 <at6DAM2@informatica.iesjc>
2022-09-27 21:10:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/pmdm/UT01_001.java
@@ -0,0 +1,14 @@
+package com.example.pmdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UT01_001 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ut01001);
+    }
+}
